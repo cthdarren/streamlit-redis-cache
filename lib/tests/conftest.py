7e8df1958c9bf572eec62e64b0c2a52aa50fd4a5
Ken McGrady <ken.mcgrady@gmail.com>
2025-01-28 16:32:12 -0800

    Do not reset DefaultStrategy

--- lib/tests/conftest.py
+++ lib/tests/conftest.py
@@ -94,9 +94,9 @@ def pytest_configure(config: pytest.Config):
 
 def pytest_runtest_setup(item: pytest.Item):
     # Ensure Default Strategy is V1 to start
-    from streamlit.runtime.pages_manager import PagesManager, PagesStrategyV1
+    # from streamlit.runtime.pages_manager import PagesManager, PagesStrategyV1
 
-    PagesManager.DefaultStrategy = PagesStrategyV1
+    # PagesManager.DefaultStrategy = PagesStrategyV1
 
     is_require_integration = item.config.getoption(
         "--require-integration", default=False
